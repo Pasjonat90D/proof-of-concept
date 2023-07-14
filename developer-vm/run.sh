cb8b18482fc0ce1ee0b21a2172b903730a88e9f6
Pasjonat90D <20847880+Pasjonat90D@users.noreply.github.com>
2023-07-14 13:54:24 +0200

    Add yaml config

--- developer-vm/run.sh
+++ developer-vm/run.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-foo () {
-    echo $1
-}
-
 oldDefaultMachineFolder=$(vboxmanage list systemproperties | grep folder | cut -d" "  -f4- | xargs)
 newDefaultMachineFolder="$(pwd)/disk"
 
@@ -11,50 +7,35 @@ echo 'Disks location'
 echo '- default: '$oldDefaultMachineFolder
 echo '- developer-vm: '$newDefaultMachineFolder
 
-read -p "Continue (y/n)?" choice
+read -p "Continue (y/n)? " choice
 case "$choice" in
   y|Y )
-        echo "yes"
-        foo 'test1'
+        echo "Creating..."
+        if [ ! -d "$newDefaultMachineFolder" ]; then
+          echo "Creating folder ${newDefaultMachineFolder}..."
+          mkdir "$newDefaultMachineFolder"
+        fi
+
+        echo 'Changing default machine folder'
+        vboxmanage setproperty machinefolder $newDefaultMachineFolder
+        #
+        echo 'Creating disk'
+        vagrant up
+        echo 'Created successfully'
+        #
+        echo 'Restoring default machine folder'
+        vboxmanage setproperty machinefolder "$oldDefaultMachineFolder"
+
+        echo 'Process completed'
+
+
+#        vagrant ssh
+#        sudo apt-get update
         ;;
   n|N )
-        echo "no"
-        foo 'test2'
+        echo "No"
         ;;
   * )
-        echo "invalid"
-        foo 'test3'
+        echo "Invalid"
         ;;
 esac
-
-
-
-
-
-
-
-#
-#
-#if [ ! -d "$newDefaultMachineFolder" ]; then
-#  echo "Creating folder ${newDefaultMachineFolder}..."
-#  mkdir "$newDefaultMachineFolder"
-#fi
-#echo $newDefaultMachineFolder
-#
-#echo 'Test' $oldDefaultMachineFolder
-#
-#echo 'Przed'
-#vboxmanage setproperty machinefolder $newDefaultMachineFolder
-#vboxmanage list systemproperties | grep folder
-##
-#vagrant up
-##
-#echo 'Po'
-#vboxmanage setproperty machinefolder "$oldDefaultMachineFolder"
-#vboxmanage list systemproperties | grep folder
-#
-#echo 'END'
-#
-
-
-
