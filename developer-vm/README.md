8e94019353fd1eb384f99eb86f3d6a2535e3b017
Pasjonat90D <20847880+Pasjonat90D@users.noreply.github.com>
2023-08-27 13:34:41 +0200

    fix RabbitMQ in Kubernetes

--- developer-vm/README.md
+++ developer-vm/README.md
@@ -1,1 +1,12 @@
 # developer-vm
+
+## Kubernetes
+
+| Service          | PORT  |
+|------------------|-------|
+| RabbitMQ-broker  | 30000 |
+| RabbitMQ-manager | 30001 |
+
+### Access
+Access from host (settings.yaml): vm.network.ip:port\
+Example (default settings): http://192.168.56.4:30001/
--- developer-vm/scripts/kubernetes/extensions/rabbitmq.sh
+++ developer-vm/scripts/kubernetes/extensions/rabbitmq.sh
@@ -2,6 +2,7 @@
 
 echo "Adding RabbitMQ..."
 
+sleep 30s #waiting for kubernetes
 cat /vagrant/kubernetes/rabbitmq/deployment.yml | envsubst | kubectl apply -f -
 cat /vagrant/kubernetes/rabbitmq/broker-service.yml | envsubst | kubectl apply -f -
 cat /vagrant/kubernetes/rabbitmq/menager-service.yml | envsubst | kubectl apply -f -
